3358b6aff9a5d26fb84d7afa02833d21725b8b5e
Wence Cajucom <cajucom.w@gmail.com>
2019-08-18 20:45:47 -0700

    edited files in "Stranger Divs" fashion in an attempt (that failed) to have responsive bar heights

--- js/main.js
+++ js/main.js
@@ -1,24 +1,90 @@
-function mcDonaldBar () {
-    console.log('Clicked on McDondald');
-    alert('A share of McDonalds Corp stock is worth: $219.71')
+// function mcDonaldBar () {
+//     console.log('Clicked on McDondald');
+//     alert('A share of McDonalds Corp stock is worth: $219.71')
+// }
+
+// function wendyBar () {
+//     console.log('Clicked on Wendys');
+//     alert('A share of Wendys stock is worth: $19.65')
+// }
+
+// function chipotleBar () {
+//     console.log('Clicked on Chipotle');
+//     alert('A share of Chipotle stock is worth: $817')
+// }
+
+// function pepsiBar () {
+//     console.log('Clicked on Pepsi');
+//     alert('A share of PepsiCo stock is worth: $129.94')
+// }
+
+// function cokeBar () {
+//     console.log('Clicked on Coke');
+//     alert('A share of Coca-Cola stock is worth: $351.67')
+// }
+
+// HW 2 Code
+console.log('Running HW2');
+
+// let state = {
+let stockList = [
+        {
+            symbol: "MCD",
+            price: "218.41",
+        },
+        {
+            symbol: "WEN",
+            price: "19.65",
+        },
+        {
+            symbol: "CMG",
+            price: "817",
+        },
+        {
+            symbol: "PEP",
+            price: "817",
+        },
+        {
+            symbol: "COKE",
+            price: "351.67",
+        },
+    ];
+// };
+
+function render() {
+    console.log('rendering HW2');
+
+    // fetch the div from index.html
+    let barsInGraph = document.querySelector('.ToJavaScript');
+
+    // loop through the array of information
+    for (let stockInfo of stockList) {
+
+        // grab the prices for each stock
+        let stockPrice = stockInfo[1];
+
+        // create a new div that contains this info
+        let newDiv = document.createElement('div');
+
+        // add a class to the newDiv called .BarsInGraph
+        newDiv.classList.add('BarsInGraph');
+
+        // add values from .BarsInGraph
+        newDiv.style.height = stockPrice / 10 + '%';
+
+        console.log('newDiv created', newDiv);
+        barsInGraph.appendChild(newDiv);
+    }
 }
 
-function wendyBar () {
-    console.log('Clicked on Wendys');
-    alert('A share of Wendys stock is worth: $19.65')
-}
 
-function chipotleBar () {
-    console.log('Clicked on Chipotle');
-    alert('A share of Chipotle stock is worth: $817')
-}
+// function doFetch() {
+//     fetch('https://financialmodelingprep.com/api/v3/stock/real-time-price')
+//         .then(response => response.json())
+//         .then(stockList => {
+//             console.log(data);
+//             state.stockList = stocklist;
+//         });
+// }
 
-function pepsiBar () {
-    console.log('Clicked on Pepsi');
-    alert('A share of PepsiCo stock is worth: $129.94')
-}
-
-function cokeBar () {
-    console.log('Clicked on Coke');
-    alert('A share of Coca-Cola stock is worth: $351.67')
-}
+render();
