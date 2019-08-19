5a60796e8407e71e3b755fa2a740697ecff2ddc2
Wence Cajucom <cajucom.w@gmail.com>
2019-08-18 19:15:17 -0700

    added script tag to index.html. moved JavaScript code to main.js. site works

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function mcDonaldBar () {
+    console.log('Clicked on McDondald');
+    alert('A share of McDonalds Corp stock is worth: $219.71')
+}
+
+function wendyBar () {
+    console.log('Clicked on Wendys');
+    alert('A share of Wendys stock is worth: $19.65')
+}
+
+function chipotleBar () {
+    console.log('Clicked on Chipotle');
+    alert('A share of Chipotle stock is worth: $817')
+}
+
+function pepsiBar () {
+    console.log('Clicked on Pepsi');
+    alert('A share of PepsiCo stock is worth: $129.94')
+}
+
+function cokeBar () {
+    console.log('Clicked on Coke');
+    alert('A share of Coca-Cola stock is worth: $351.67')
+}
