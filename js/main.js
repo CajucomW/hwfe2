be8cc053853297338f2f5ce11540afeef1ff0b79
Wence Cajucom <cajucom.w@gmail.com>
2019-08-20 21:40:14 -0700

    wrote doFetch function. for loop isnt working. fetching data is not reflected on DOM

--- js/main.js
+++ js/main.js
@@ -1,65 +1,73 @@
-// function mcDonaldBar () {
-//     console.log('Clicked on McDondald');
-//     alert('A share of McDonalds Corp stock is worth: $219.71')
-// }
-
-// function wendyBar () {
-//     console.log('Clicked on Wendys');
-//     alert('A share of Wendys stock is worth: $19.65')
-// }
-
-// function chipotleBar () {
-//     console.log('Clicked on Chipotle');
-//     alert('A share of Chipotle stock is worth: $817')
-// }
-
-// function pepsiBar () {
-//     console.log('Clicked on Pepsi');
-//     alert('A share of PepsiCo stock is worth: $129.94')
-// }
-
-// function cokeBar () {
-//     console.log('Clicked on Coke');
-//     alert('A share of Coca-Cola stock is worth: $351.67')
-// }
-
-// HW 2 Code
 console.log('Running HW2');
 
-// let state = {
-let stockList = [
+let state = {
+    stockList: [
         {
-            symbol: "MCD",
-            price: "218.41",
+            symbol: "Wala",
+            price: "Wala",
         },
-        {
-            symbol: "WEN",
-            price: "19.65",
-        },
-        {
-            symbol: "CMG",
-            price: "817",
-        },
-        {
-            symbol: "PEP",
-            price: "132.35",
-        },
-        {
-            symbol: "COKE",
-            price: "351.67"
-        }
-    ];
-// };
+    ],
+};
 
-function render() {
-    console.log('rendering HW2');
+// function render() {
+//     console.log('rendering HW2');
 
-    // fetch the div from index.html
+//     // fetch the div from index.html
+//     let barsInGraph = document.querySelector('#ToJavaScript');
+//     console.log('barsInGraph defined');
+
+//     // loop through the array of information
+//     for (let stockInfo of stockList) {
+//         console.log('for loop activated');
+
+//         // grab the prices and symbols for each stock
+//         let stockPrice = stockInfo.price;
+//         console.log('stockPrice defined');
+//         // grab the names for each stock
+//         let stockName = stockInfo.symbol;
+//         console.log('stockName defined');
+
+//         // create a new div that contains this info
+//         let newDiv = document.createElement('div');
+//         console.log('newDiv defined');
+
+//         // // create a new h2 that contains the symbol
+//         // let newH2 = document.createElement('h2');
+//         // console.log('newH2 defined');
+
+//         // add a class to the newDiv called .BarsInGraph
+//         newDiv.classList.add('BarsInGraph');
+//         console.log('BarsInGraph added in newDiv');
+//         newDiv.textContent = stockName + " - $" + stockPrice;
+        
+//         //add a class to the newH2 called .BarNamePosition
+//         // newH2.textContent = stockName;
+//         // console.log('BarNamePosition added in newH2');
+
+//         // add values from .BarsInGraph
+//         newDiv.style.height = stockPrice / 10 + '%';
+//         console.log('height established');
+
+//         // if (newDiv.style.height < 10) {
+//         //     let newH2 = document.createElement('h2');
+//         //     newH2.textContent = stockName;
+//         //     newH2.classList.add('BarNamePosition');
+//         // }
+
+//         console.log('newDiv created', newDiv);
+//         barsInGraph.appendChild(newDiv);    
+//     }
+// }
+
+//_____________________________________________________API
+
+function displayBarGraph() {
+    console.log('running displayBarGraph');
+
     let barsInGraph = document.querySelector('#ToJavaScript');
     console.log('barsInGraph defined');
 
-    // loop through the array of information
-    for (let stockInfo of stockList) {
+    for (let stockInfo of state.stockList) {
         console.log('for loop activated');
 
         // grab the prices and symbols for each stock
@@ -73,36 +81,29 @@ function render() {
         let newDiv = document.createElement('div');
         console.log('newDiv defined');
 
-        // // create a new h2 that contains the symbol
-        // let newH2 = document.createElement('h2');
-        // console.log('newH2 defined');
-
         // add a class to the newDiv called .BarsInGraph
         newDiv.classList.add('BarsInGraph');
         console.log('BarsInGraph added in newDiv');
-        newDiv.textContent = stockName;
-
-        //add a class to the newH2 called .BarNamePosition
-        // newH2.textContent = stockName;
-        // console.log('BarNamePosition added in newH2');
+        newDiv.textContent = stockName + " - $" + stockPrice;
 
         // add values from .BarsInGraph
         newDiv.style.height = stockPrice / 10 + '%';
         console.log('height established');
 
         console.log('newDiv created', newDiv);
-        barsInGraph.appendChild(newDiv);    
+        barsInGraph.appendChild(newDiv);
     }
+
 }
 
+function doFetch() {
+    fetch('https://financialmodelingprep.com/api/v3/stock/real-time-price')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            state.stockList = data;
+        });
+}
 
-// function doFetch() {
-//     fetch('https://financialmodelingprep.com/api/v3/stock/real-time-price')
-//         .then(response => response.json())
-//         .then(stockList => {
-//             console.log(data);
-//             state.stockList = stocklist;
-//         });
-// }
-
-render();
+displayBarGraph();
+doFetch();
