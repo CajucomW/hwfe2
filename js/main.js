4b04b7a351e56cd4f2227e61d091b6e97432952c
Wence Cajucom <cajucom.w@gmail.com>
2019-08-23 12:13:11 -0700

    clean comments out of code. start checkboxes

--- js/main.js
+++ js/main.js
@@ -1,53 +1,3 @@
-// function render() {
-//     console.log('rendering HW2');
-
-//     // fetch the div from index.html
-//     let barsInGraph = document.querySelector('#ToJavaScript');
-//     console.log('barsInGraph defined');
-
-//     // loop through the array of information
-//     for (let stockInfo of stockList) {
-//         console.log('for loop activated');
-
-//         // grab the prices and symbols for each stock
-//         let stockPrice = stockInfo.price;
-//         console.log('stockPrice defined');
-//         // grab the names for each stock
-//         let stockName = stockInfo.symbol;
-//         console.log('stockName defined');
-
-//         // create a new div that contains this info
-//         let newDiv = document.createElement('div');
-//         console.log('newDiv defined');
-
-//         // // create a new h2 that contains the symbol
-//         // let newH2 = document.createElement('h2');
-//         // console.log('newH2 defined');
-
-//         // add a class to the newDiv called .BarsInGraph
-//         newDiv.classList.add('BarsInGraph');
-//         console.log('BarsInGraph added in newDiv');
-//         newDiv.textContent = stockName + " - $" + stockPrice;
-        
-//         //add a class to the newH2 called .BarNamePosition
-//         // newH2.textContent = stockName;
-//         // console.log('BarNamePosition added in newH2');
-
-//         // add values from .BarsInGraph
-//         newDiv.style.height = stockPrice / 10 + '%';
-//         console.log('height established');
-
-//         // if (newDiv.style.height < 10) {
-//         //     let newH2 = document.createElement('h2');
-//         //     newH2.textContent = stockName;
-//         //     newH2.classList.add('BarNamePosition');
-//         // }
-
-//         console.log('newDiv created', newDiv);
-//         barsInGraph.appendChild(newDiv);    
-//     }
-// }
-
 console.log('Running HW2');
 
 let state = {
@@ -75,6 +25,7 @@ function displayBarGraph() {
         // grab the prices and symbols for each stock
         let stockPrice = stockInfo.price;
         console.log('stockPrice defined');
+
         // grab the names for each stock
         let stockName = stockInfo.symbol;
         console.log('stockName defined');
@@ -92,6 +43,7 @@ function displayBarGraph() {
         // add a class to the newDiv called .BarsInGraph
         newDiv.classList.add('BarsInGraph');
         console.log('BarsInGraph added in newDiv');
+        
         // add values from .BarsInGraph
         newDiv.style.height = stockPrice / 10 + '%';
         console.log('height established');
