34be4f0b1a13de6e9d59d426e333f7868faaa1ad
Wence Cajucom <cajucom.w@gmail.com>
2019-08-23 19:00:56 -0700

    edit to README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hwfe2
-JavaScript Chart
+JavaScript Chart.
+
+This bar graph contains stock prices of ten famous establishments and companies.
+Stock prices are collected from <em>https://financialmodelingprep.com/</em> API.
--- js/main.js
+++ js/main.js
@@ -13,43 +13,33 @@ let state = {
 //_____________________________________________________API
 
 function displayBarGraph() {
-    console.log('running displayBarGraph');
 
     let barsInGraph = document.querySelector('#ToJavaScript');
     barsInGraph.innerHTML = '';
-    console.log('barsInGraph defined');
 
     for (let stockInfo of state.stockList) {
-        console.log('for loop activated');
 
         // grab the prices and symbols for each stock
         let stockPrice = stockInfo.price;
-        console.log('stockPrice defined');
 
         // grab the names for each stock
         let stockName = stockInfo.symbol;
-        console.log('stockName defined');
 
         // create a new div that contains this info
         let newDiv = document.createElement('div');
-        console.log('newDiv defined');
         
         //create a new h2 that contains the symbol
         let newH3 = document.createElement('h3');
         newH3.textContent = stockName + " - $" + stockPrice;
-        console.log('newH3 defined');
         newH3.classList.add('BarNamePosition');
 
         // add a class to the newDiv called .BarsInGraph
         newDiv.classList.add('BarsInGraph');
-        console.log('BarsInGraph added in newDiv');
 
         // add values from .BarsInGraph
         newDiv.style.height = stockPrice / 10 + '%';
-        console.log('height established');
 
-        console.log('newDiv created', newDiv, 
-        'new h3 created', newH3);
+        // append new stuff
         newDiv.appendChild(newH3);
         barsInGraph.appendChild(newDiv);
         
@@ -73,7 +63,6 @@ function doFetch() {
     fetch('https://financialmodelingprep.com/api/v3/stock/real-time-price')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             state.stockList = data.stockList.filter(stockdata => 
             stockdata.symbol === "CMG" || 
             stockdata.symbol === "MCD" || 
@@ -88,7 +77,6 @@ function doFetchAgain() {
     fetch('https://financialmodelingprep.com/api/v3/stock/real-time-price')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             state.stockList = data.stockList.filter(stockdata => 
             stockdata.symbol === "CMG" || 
             stockdata.symbol === "MCD" || 
