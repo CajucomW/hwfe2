3060d951877a19deb044049ec4809ae9685ae7e3
Wence Cajucom <cajucom.w@gmail.com>
2019-08-23 18:38:24 -0700

    create buttons for 10 stocks.

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ function displayBarGraph() {
         // create a new div that contains this info
         let newDiv = document.createElement('div');
         console.log('newDiv defined');
-
+        
         //create a new h2 that contains the symbol
         let newH3 = document.createElement('h3');
         newH3.textContent = stockName + " - $" + stockPrice;
@@ -43,18 +43,31 @@ function displayBarGraph() {
         // add a class to the newDiv called .BarsInGraph
         newDiv.classList.add('BarsInGraph');
         console.log('BarsInGraph added in newDiv');
-        
+
         // add values from .BarsInGraph
         newDiv.style.height = stockPrice / 10 + '%';
         console.log('height established');
 
-        console.log('newDiv created', newDiv, 'new h2 created', newH3);
+        console.log('newDiv created', newDiv, 
+        'new h3 created', newH3);
         newDiv.appendChild(newH3);
         barsInGraph.appendChild(newDiv);
         
     }
-
 }
+
+function displayNameButtons() {
+    let stockSymbolNames = document.querySelector('#CheckBoxToJavaScript');
+    
+    for (let stockSymbol of state.stockList) {
+        let stockName = stockSymbol.symbol;
+        let newCheckBox = document.createElement('button');
+        newCheckBox.textContent = stockName;
+        stockSymbolNames.appendChild(newCheckBox);
+    }
+}
+
+
 
 function doFetch() {
     fetch('https://financialmodelingprep.com/api/v3/stock/real-time-price')
@@ -71,4 +84,25 @@ function doFetch() {
         });
 }
 
+function doFetchAgain() {
+    fetch('https://financialmodelingprep.com/api/v3/stock/real-time-price')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            state.stockList = data.stockList.filter(stockdata => 
+            stockdata.symbol === "CMG" || 
+            stockdata.symbol === "MCD" || 
+            stockdata.symbol === "PEP" || 
+            stockdata.symbol === "WEN" || 
+            stockdata.symbol === "COKE" ||
+            stockdata.symbol === "AMZN" ||
+            stockdata.symbol === "GOOGL" ||
+            stockdata.symbol === "IBM" ||
+            stockdata.symbol === "AAPL" ||
+            stockdata.symbol === "MSFT");
+            displayNameButtons();
+        });
+}
+
 doFetch();
+doFetchAgain();
